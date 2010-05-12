5fc5ead0a56ddef389b9ba02a120135941197fba
diegofv <diegofv@af25a85e-aff4-9f4e-7d77-88623c0fc43b>
2010-05-12 16:39:49 +0000

    esqueleto actualizado

--- src/Cliente.java
+++ src/Cliente.java
@@ -12,11 +12,37 @@ import java.net.SocketException;
 
 public class Cliente {
 
-	
+	private Mensaje mensajeRecibido;
 	
 	public Cliente() throws SocketException
 	{
 		DatagramSocket s = new DatagramSocket(null);
 		s.bind(new InetSocketAddress(8888));
+	}
+	
+	/**
+	 * Solicita direccion ip
+	 */
+	private void solicitar() 
+	{
+		//la direccion llega en un mensaje clase Mensaje attrib mnesajeRecibido
+		
+	}
+	
+	/**
+	 * Valida direccion ip (valida que tenga todos los datos
+	 */
+	private void valida()
+	{
+		
+	}
+	
+	/**
+	 * Confiura la direccion en el pc (LINUX)
+	 */
+	private void configuraDireccion()
+	{
+		//coje info del mensaje
+		//establece en el cliente el gateware, dns, ip y mask (WINS)
 	}
 }
