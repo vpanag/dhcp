bbac593ce6c0ec574148723cc08859f438e3b378
diegofv <diegofv@af25a85e-aff4-9f4e-7d77-88623c0fc43b>
2010-05-26 17:11:08 +0000

    clase servidormain

--- trunk/src/ServidorMain.java
+++ trunk/src/ServidorMain.java
@@ -1,12 +1,26 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
 
 public class ServidorMain {
 
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		DatagramSocket datagramSocket = new DatagramSocket();
+
+		byte[] buffer = "0123456789".getBytes();
+		InetAddress receiverAddress = InetAddress.getLocalHost();
+
+		DatagramPacket packet = new DatagramPacket(
+		        buffer, buffer.length, receiverAddress, 80);
+		datagramSocket.send(packet);
 
 	}
-
 }
